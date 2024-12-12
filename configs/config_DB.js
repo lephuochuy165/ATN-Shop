37cc35b6eba91ec0e9d0adb56e4a038f57fd4ffb
lephuochuy165 <84957598+lephuochuy165@users.noreply.github.com>
2024-12-12 18:53:42 +0700

    First Commit

---
+++ configs/config_DB.js
@@ -0,0 +1,9 @@
+module.exports = {
+    clouddb: {
+        urldb : 'mongodb+srv://huylpgcs18720:mi7NDMHp7bablxvN@cluster0.t4lrva0.mongodb.net/atnshop?retryWrites=true&w=majority',
+        dbname : "atnshop",
+        dbusername : "",
+        dbpassword : "mi7NDMHp7bablxvN",
+    }
+
+};
